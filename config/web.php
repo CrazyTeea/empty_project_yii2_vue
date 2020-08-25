a735c43c1a712a681b7e7a447bb5ebff824a9061
CrazyTea <lipatow.nikita@yandex.ru>
2020-08-25 15:27:52 +0300

    added fontawesome

--- config/web.php
+++ config/web.php
@@ -2,6 +2,7 @@
 
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
+$routes = require __DIR__ . '/router.php';
 
 $config = [
     'id' => 'basic',
@@ -68,9 +69,7 @@ $config = [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-                'main'=>'app/test'
-            ],
+            'rules' => $routes
         ],
 
     ],
